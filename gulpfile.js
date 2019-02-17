d8251e01725fd3854ea5d6b106a06bd219094819
asaparov <abulhair.saparov@gmail.com>
2019-02-17 14:54:41 -0500

    Updated for gulp version 4.

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,6 @@ var notify			= require('gulp-notify');
 var postcss			= require('gulp-postcss');
 var rename			= require('gulp-rename');
 var run				= require('gulp-run');
-var runSequence		= require('run-sequence');
 var sass			= require('gulp-ruby-sass');
 var uglify			= require('gulp-uglify');
 var htmlmin			= require('gulp-htmlmin');
@@ -47,7 +46,7 @@ gulp.task('build:styles:critical', function() {
 });
 
 // Builds all styles.
-gulp.task('build:styles', ['build:styles:main', 'build:styles:critical']);
+gulp.task('build:styles', gulp.parallel('build:styles:main', 'build:styles:critical'));
 
 gulp.task('clean:styles', function(callback) {
 	del([paths.siteCssFiles + '/style.css',
@@ -73,26 +72,8 @@ gulp.task('clean:scripts', function(callback) {
 		.then(function(result) {callback();});
 });
 
-// Concatenates and uglifies leaflet JS files and outputs result to the
-// appropriate location.
-gulp.task('build:scripts:leaflet', function() {
-	return gulp.src([
-		paths.jsFiles + '/leaflet/leaflet.js',
-		paths.jsFiles + '/leaflet/leaflet-providers.js'
-	])
-		.pipe(concat('leaflet.js'))
-		.pipe(uglify())
-		.pipe(gulp.dest(paths.siteJsFiles))
-		.on('error', gutil.log);
-});
-
-gulp.task('clean:scripts:leaflet', function(callback) {
-	del([paths.siteJsFiles + '/leaflet.js'])
-		.then(function(result) {callback();});
-});
-
 // Builds all scripts.
-gulp.task('build:scripts', ['build:scripts:global', 'build:scripts:leaflet']);
+gulp.task('build:scripts', gulp.parallel('build:scripts:global'));
 
 // Optimizes and copies image files.
 gulp.task('build:images', function() {
@@ -116,9 +97,6 @@ gulp.task('clean:assets', function(callback) {
 		.then(function(result) {callback();});
 });
 
-// Copies fonts.
-gulp.task('build:fonts', ['fontawesome']);
-
 // Places Font Awesome fonts in proper location.
 gulp.task('fontawesome', function() {
 	return gulp.src(paths.fontFiles + '/font-awesome/**.*')
@@ -127,13 +105,16 @@ gulp.task('fontawesome', function() {
 		.on('error', gutil.log);
 });
 
+// Copies fonts.
+gulp.task('build:fonts', gulp.series('fontawesome'));
+
 gulp.task('clean:fonts', function(callback) {
 	del([paths.siteFontFiles])
 		.then(function(result) {callback();});
 });
 
 gulp.task('html', function() {
-	gulp.src(paths.srcHtmlFolderName + '/**/*.html')
+	return gulp.src(paths.srcHtmlFolderName + '/**/*.html')
 		.pipe(inliner({ rootpath : paths.srcHtmlFolderName }))
 		.pipe(htmlmin({collapseWhitespace: true}))
 		.pipe(gulp.dest(paths.siteDir));
@@ -145,8 +126,7 @@ gulp.task('build:docs:xml', function (callback) {
 });
 
 gulp.task('build:docs:html', function() {
-	return gulp.src('')
-		.pipe(run('python make_docs.py'))
+	return run('python make_docs.py').exec()
 		.on('error', gutil.log);
 });
 
@@ -156,22 +136,21 @@ gulp.task('clean:docs', function(callback) {
 		.then(function(result) {callback();});
 });
 
-gulp.task('clean', ['clean:docs',
+gulp.task('clean', gulp.parallel(
+	'clean:docs',
 	'clean:fonts',
 	'clean:images',
 	'clean:assets',
 	'clean:scripts',
-	'clean:styles']);
+	'clean:styles'));
 
 // Builds site anew.
-gulp.task('build', function(callback) {
-	runSequence('clean', 'build:docs:xml', 'build:docs:html',
-		['build:scripts', 'build:images', 'build:assets', 'build:styles', 'build:fonts'],
-		'html', callback);
-});
+gulp.task('build', gulp.series('clean', 'build:docs:xml', 'build:docs:html',
+		gulp.parallel('build:scripts', 'build:images', 'build:assets', 'build:styles', 'build:fonts'),
+		'html'));
 
 // Default Task: builds site.
-gulp.task('default', ['build']);
+gulp.task('default', gulp.series('build'));
 
 // Updates Ruby gems
 gulp.task('update:bundle', function() {
